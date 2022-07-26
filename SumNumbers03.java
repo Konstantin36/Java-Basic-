65cfd72425e064bf9c78da9a04c721001e2e4dcf
Konstantin <koce123456789koce@gmail.com>
2022-07-26 23:18:43 +0300

    Add files via upload

---
+++ SumNumbers03.java
@@ -0,0 +1,19 @@
+package LAB;
+
+import java.util.Scanner;
+
+public class SumNumbers03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+
+        int sum = 0;
+
+        while (sum < n){
+            int currentNum = Integer.parseInt(scan.nextLine());
+            sum += currentNum;
+        }
+        System.out.println(sum);
+    }
+}
